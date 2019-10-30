a0866c50a0a7d2d7a15b3a57bacb2677b2ea9e5d
Nazim Boudeffa <boudeffa@gmail.com>
2019-10-30 21:29:20 +0100

    Cleaning code

--- search.php
+++ search.php
@@ -14,114 +14,27 @@ $obj = YouPHPTubePlugin::getObjectData("Wasaaa");
         <?php
         include $global['systemRootPath'] . 'view/include/head.php';
         ?>
-        <style>
-            #custom-search-input{
-                padding: 3px;
-                border: solid 1px #E4E4E4;
-                border-radius: 6px;
-                background-color: #fff;
-            }
-
-            #custom-search-input input{
-                border: 0;
-                box-shadow: none;
-            }
-
-            #custom-search-input button{
-                margin: 2px 0 0 0;
-                background: none;
-                box-shadow: none;
-                border: 0;
-                color: #666666;
-                padding: 0 8px 0 10px;
-                border-left: solid 1px #ccc;
-            }
-
-            #custom-search-input button:hover{
-                border: 0;
-                box-shadow: none;
-                border-left: solid 1px #ccc;
-            }
-
-            #custom-search-input .glyphicon-search{
-                font-size: 23px;
-            }
-            #results li {
-                padding: 10px 0;
-                border-bottom: 1px dotted #ccc;
-                list-style: none;
-                overflow: auto;
-            }
-            .list-left {
-                float: left;
-                width: 20%;
-            }
-            .list-left img {
-                width: 100%;
-                padding: 3px;
-                border: 1px solid #ccc;
-            }
-            .list-right {
-                float: right;
-                width: 78%;
-            }
-            .list-right h3 {
-                margin: 0;
-            }
-            .list-right p {
-                margin: 0;
-            }
-
-            .cTitle {
-                color: #dd2826;
-            }
-
-            .button-container {
-                margin-top: 25px;
-
-            }
-
-            .paging-button {
-                background: #f4f4f4;
-                padding: 0 13px;
-                border: #ccc 1px solid;
-                border-radius: 5px;
-                color: #333;
-                margin: 10px;
-                cursor: pointer;
-            }
-        </style>
     </head>
     <body class="<?php echo $global['bodyClass']; ?>">
         <?php
         include $global['systemRootPath'] . 'view/include/navbar.php';
         ?>
         <div class="container">
-
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <form id="search-form" name="search-form" onsubmit="return search()">
-                        <div id="custom-search-input">
-                            <div class="input-group col-md-12">
-                              <!--
-                                <input type="search" id="query" class="form-control input-lg" placeholder="Search Your Wasabi Buckets" />
-                                <span class="input-group-btn">
-                                    <button class="btn btn-info btn-lg" type="submit">
-                                        <i class="glyphicon glyphicon-search"></i>
-                                    </button>
-                                </span>
-                              -->
-                            </div>
+                    <div id="select-input">
+                        <div class="input-group col-md-12">
+                          <!-- Select -->
                         </div>
-                    </form>
+                    </div>
                     <br>
                     <div class="row">
-                        <div class="col-sm-6">
-                            <button class="btn btn-info btn-block" id="getAll"><?php echo __('Embed All'); ?></button>
-                        </div>
-                        <div class="col-sm-6">
-                            <button class="btn btn-success btn-block" id="getSelected"><?php echo __('Embed Selected'); ?></button>
-                        </div>
+                      <div class="col-sm-6">
+                          <button class="btn btn-success btn-block" id="getSelected"><?php echo __('Embed Selected'); ?></button>
+                      </div>
+                      <div class="col-sm-6">
+                          <button class="btn btn-info btn-block" id="getAll"><?php echo __('Embed All'); ?></button>
+                      </div>
                     </div>
                 </div>
                 <div class="panel-body">
@@ -159,13 +72,13 @@ $obj = YouPHPTubePlugin::getObjectData("Wasaaa");
                });
 
                var sel = $('<select>').appendTo('body');
-                sel.append($("<option>").text("Please select a bucket").prop('disabled', true).prop('selected', true));
+                sel.append($("<option>").text("Select a bucket").prop('disabled', true).prop('selected', true));
                buckets.forEach(function(element) {
                 sel.append($("<option>").attr('value',element.bucket).text(element.bucket));
                });
                $(sel).attr('id', "buckets");
                $(sel).addClass('form-control');
-               $('#custom-search-input').append(sel);
+               $('#select-input').append(sel);
              }
             });
 
